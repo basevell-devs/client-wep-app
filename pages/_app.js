bc0f8dc6b3c0e627c1510532b0907979cb28804f
Mahdi <muhdmahdia36@gmail.com>
2022-10-02 22:00:08 +0100

    Some cahnges made

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,15 @@ import { theme } from "../themes/theme";
 import { AuthProvider } from "../firebase/context";
 import { AppProvider } from "../context/AppProvider";
 
+import Loader from "../components/Loader";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <AppProvider>
           <CssBaseline />
+          <Loader />
           <Component {...pageProps} />
         </AppProvider>
       </AuthProvider>
